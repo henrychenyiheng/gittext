8b563593d06690c5177ac6e9a0604194ab48ed26
HenryChen <44835574+henrychenyiheng@users.noreply.github.com>
2018-11-12 11:45:42 +0800

    text

---
+++ WebApplication1text/WebApplication1/WebApplication1/Migrations/20181107062526_Initial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    ID = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    ISNB = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
---
+++ WebApplication1text/WebApplication1/WebApplication1/Models/Book.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using WebApplication1.Models;
+
+namespace WebApplication1.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Books> Books { get; set; }
+
+        public DbSet<WebApplication1.Models.Book> Book { get; set; }
+    }
+
+    public class Books
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string ISNB { get; set; }
+    }
+
+    public class Book
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string ISNB { get; set; }
+
+    }
+}
